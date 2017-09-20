e29ad46eaa66bb1b16ed7f1c73970ce6f1769252
Zack <Zack@NN.COM>
2017-09-20 17:45:00 +0800

    add all to admin

--- src/game/admin.py
+++ src/game/admin.py
@@ -6,7 +6,32 @@ from .models import Player, Game, Stage, Group, Battle, Match
 
 
 class PlayerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "sex", "age", "height", "qq")
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("name", "start_date", "end_date", "result", "champion", "second_place")
+
+
+class StageAdmin(admin.ModelAdmin):
+    list_display = ("game", "name")
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("stage", "name")
+
+
+class BattleAdmin(admin.ModelAdmin):
+    list_display = ("group", "player1", "player2", "winner")
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ("battle", "name")
 
 
 admin.site.register(Player, PlayerAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Stage, StageAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Battle, BattleAdmin)
+admin.site.register(Match, MatchAdmin)
--- src/game/models.py
+++ src/game/models.py
@@ -5,13 +5,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Player(models.Model):
     name = models.CharField(max_length=30, verbose_name=u'选手名称')
     SEX_CHOICES = (
-        ('F', 'Female'),
-        ('M', 'Male'),
+        ('F', u'女'),
+        ('M', u'男'),
     )
-    sex = models.CharField(max_length=1, choices=SEX_CHOICES, verbose_name=u'选手性别')
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, verbose_name=u'选手性别', default='M')
     age = models.IntegerField(verbose_name=u'年龄')
     height = models.IntegerField(verbose_name=u'身高')
     qq = models.IntegerField(verbose_name='QQ粉丝群')
@@ -27,9 +28,11 @@ class Game(models.Model):
     name = models.CharField(max_length=30, verbose_name=u'比赛名称')
     start_date = models.DateField(u'开始日期')
     end_date = models.DateField(u'结束日期')
-    result = models.CharField(max_length=128, verbose_name=u'比赛排名')
-    champion = models.CharField(max_length=30, verbose_name=u'冠军')
-    second_place = models.CharField(max_length=30, verbose_name=u'亚军')
+    result = models.CharField(max_length=128, null=True, blank=True, verbose_name=u'比赛排名')
+    champion = models.ForeignKey(Player, max_length=30, null=True,
+                                 blank=True, verbose_name=u'冠军', related_name='champion')
+    second_place = models.ForeignKey(
+        Player, max_length=30, null=True, blank=True, verbose_name=u'亚军', related_name='second_place')
 
     def __unicode__(self):
         return self.name
@@ -65,21 +68,22 @@ class Battle(models.Model):
     group = models.ForeignKey(Group, verbose_name=u'小组名称')
     player1 = models.ForeignKey(Player, verbose_name=u'选手1', related_name='player1')
     player2 = models.ForeignKey(Player, verbose_name=u'选手2', related_name='player2')
-    winner = models.ForeignKey(Player, verbose_name=u'胜者', related_name='winner')
+    winner = models.ForeignKey(Player, null=True, blank=True,
+                               verbose_name=u'胜者', related_name='winner')
 
     # def __unicode__(self):
     #     return self.name
 
     class Meta:
-        verbose_name_plural = u"对战"
+        verbose_name_plural = u"对抗"
 
 
 class Match(models.Model):
     battle = models.ForeignKey(Battle, verbose_name=u'阶段名称')
-    name = models.CharField(max_length=30, verbose_name=u'组别名称')
+    name = models.CharField(max_length=30, verbose_name=u'对战名称')
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = u"组别"
+        verbose_name_plural = u"对战"
